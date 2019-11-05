d2899c4e82a893fc75155ae45b147afc96bcb65b
babak35102 <babak35102@gmail.com>
2019-11-05 08:34:49 +0330

    Raw Project

---
+++ Kala/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kala.Startup))]
+namespace Kala
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
